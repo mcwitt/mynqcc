5d70e8545aa6e77d7a67800fc6b15496324d1f4a
Matt Wittmann <mcwitt@gmail.com>
2018-10-11 15:10:35 -0700

    Add Token data type and stub lex function

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,31 @@
 module Lib
-    ( someFunc
+    ( Lib.lex
+    , Token (..)
+    , someFunc
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+data Token = OpenBrace
+           | CloseBrace
+           | OpenParen
+           | CloseParen
+           | Semicolon
+           | KWInt
+           | KWReturn
+           | Identifier String
+           | Integer Int
+           deriving (Show, Eq)
+
+lex :: String -> [Token]
+lex _ = [ KWInt
+        , Identifier "main"
+        , OpenParen
+        , CloseParen
+        , OpenBrace
+        , KWReturn
+        , Integer 100
+        , Semicolon
+        , CloseBrace
+        ]
