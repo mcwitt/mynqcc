9c59cddf7a76efd690c5a5c23c7933e872330e08
Matt Wittmann <mcwitt@gmail.com>
2018-10-11 15:10:13 -0700

    Add first test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,38 @@
+{-# LANGUAGE RecordWildCards #-}
+
+import Data.Foldable     (for_)
+import Test.Hspec        (Spec, describe, it, shouldBe)
+import Test.Hspec.Runner (configFastFail, defaultConfig, hspecWith)
+
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspecWith defaultConfig {configFastFail = True} specs
+
+specs :: Spec
+specs = describe "lex" $ for_ cases test
+  where
+    test Case {..} = it description $ Lib.lex input `shouldBe` expected
+
+data Case = Case { description :: String
+                 , input       :: String
+                 , expected    :: [Token]
+                 }
+
+cases :: [Case]
+cases = [ Case { description = "basic"
+               , input       = "int main() {\
+                               \    return 100;\
+                               \}"
+               , expected    = [ KWInt
+                               , Identifier "main"
+                               , OpenParen
+                               , CloseParen
+                               , OpenBrace
+                               , KWReturn
+                               , Integer 100
+                               , Semicolon
+                               , CloseBrace
+                               ]
+               }
+        ]
