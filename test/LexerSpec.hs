c5986865aab477ce80ae7dc46ce65abd76794a33
Matt Wittmann <mcwitt@gmail.com>
2018-10-15 21:57:48 -0700

    Refactor tests

--- test/LexerSpec.hs
+++ test/LexerSpec.hs
@@ -1,196 +1,179 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module LexerSpec (spec) where
 
-import Data.Foldable     (for_)
-import Test.Hspec        (Spec, describe, it, shouldBe)
-
+import Test.Hspec ( Spec
+                  , describe
+                  , it
+                  , shouldBe)
 
 import Lexer (Token (..), lex)
 
 spec :: Spec
-spec = do describe "lexer: valid"   $ for_ validCases test
-          describe "lexer: invalid" $ for_ invalidCases test
-  where
-    test Case  {..} = it description $ Lexer.lex input `shouldBe` expected
+spec = do
+  it "should lex multi_digit.c" $ do
+    Lexer.lex "int main() {\n\
+              \    return 100;\n\
+              \}"
+      `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenParen
+      , CloseParen
+      , OpenBrace
+      , KWReturn
+      , Integer 100
+      , Semicolon
+      , CloseBrace
+      ]
 
-data Case = Case { description :: String
-                 , input       :: String
-                 , expected    :: [Token]
-                 }
+  it "should lex newlines.c" $ do
+    Lexer.lex "int\n\
+              \main\n\
+              \(\n\
+              \)\n\
+              \{\n\
+              \return\n\
+              \0\n\
+              \;\n\
+              \}"
+      `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenParen
+      , CloseParen
+      , OpenBrace
+      , KWReturn
+      , Integer 0
+      , Semicolon
+      , CloseBrace
+      ]
 
-validCases :: [Case]
-validCases = [
-  Case { description = "multi_digit.c"
-       , input       = "int main() {\n\
-                       \    return 100;\n\
-                                         \}"
-       , expected    = [ KWInt
-                       , Identifier "main"
-                       , OpenParen
-                       , CloseParen
-                       , OpenBrace
-                       , KWReturn
-                       , Integer 100
-                       , Semicolon
-                       , CloseBrace
-                       ]
-       }
+  it "should lex no_newlines.c" $ do
+    Lexer.lex "int main(){return 0;}" `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenParen
+      , CloseParen
+      , OpenBrace
+      , KWReturn
+      , Integer 0
+      , Semicolon
+      , CloseBrace
+      ]
 
-  , Case { description = "newlines.c"
-         , input       = "int\n\
-                         \main\n\
-                         \(\n\
-                         \)\n\
-                         \{\n\
-                         \return\n\
-                         \0\n\
-                         \;\n\
-                         \}"
-         , expected    = [ KWInt
-                         , Identifier "main"
-                         , OpenParen
-                         , CloseParen
-                         , OpenBrace
-                         , KWReturn
-                         , Integer 0
-                         , Semicolon
-                         , CloseBrace
-                         ]
-         }
+  it "should lex return_0.c" $ do
+    Lexer.lex "int main() {\n\
+              \    return 0;\n\
+              \}"
+      `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenParen
+      , CloseParen
+      , OpenBrace
+      , KWReturn
+      , Integer 0
+      , Semicolon
+      , CloseBrace
+      ]
 
-  , Case { description = "no_newlines.c"
-         , input       = "int main(){return 0;}"
-         , expected    = [ KWInt
-                         , Identifier "main"
-                         , OpenParen
-                         , CloseParen
-                         , OpenBrace
-                         , KWReturn
-                         , Integer 0
-                         , Semicolon
-                         , CloseBrace
-                         ]
-         }
+  it "should lex return_2.c" $ do
+    Lexer.lex "int main() {\n\
+              \    return 2;\n\
+              \}"
+      `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenParen
+      , CloseParen
+      , OpenBrace
+      , KWReturn
+      , Integer 2
+      , Semicolon
+      , CloseBrace
+      ]
 
-  , Case { description = "return_0.c"
-         , input       = "int main() {\n\
-                         \    return 0;\n\
-                         \}"
-         , expected    = [ KWInt
-                         , Identifier "main"
-                         , OpenParen
-                         , CloseParen
-                         , OpenBrace
-                         , KWReturn
-                         , Integer 0
-                         , Semicolon
-                         , CloseBrace
-                         ]
-         }
+  it "should lex spaces.c" $ do
+    Lexer.lex "int   main    (  )  {   return  0 ; }"
+      `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenParen
+      , CloseParen
+      , OpenBrace
+      , KWReturn
+      , Integer 0
+      , Semicolon
+      , CloseBrace
+      ]
 
-  , Case { description = "return_2.c"
-         , input       = "int main() {\n\
-                         \    return 2;\n\
-                         \}"
-         , expected    = [ KWInt
-                         , Identifier "main"
-                         , OpenParen
-                         , CloseParen
-                         , OpenBrace
-                         , KWReturn
-                         , Integer 2
-                         , Semicolon
-                         , CloseBrace
-                         ]
-         }
+  it "should lex missing_paren.c" $ do
+    Lexer.lex "int main( {\n\
+              \    return 0;\n\
+              \}"
+      `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenParen
+      , OpenBrace
+      , KWReturn
+      , Integer 0
+      , Semicolon
+      , CloseBrace
+      ]
 
-  , Case { description = "spaces.c"
-         , input       = "int   main    (  )  {   return  0 ; }"
-         , expected    = [ KWInt
-                         , Identifier "main"
-                         , OpenParen
-                         , CloseParen
-                         , OpenBrace
-                         , KWReturn
-                         , Integer 0
-                         , Semicolon
-                         , CloseBrace
-                         ]
-         }
-  ]
+  it "should lex missing_retval.c" $ do
+    Lexer.lex "int main() {\n\
+              \    return;\n\
+              \}"
+      `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenParen
+      , CloseParen
+      , OpenBrace
+      , KWReturn
+      , Semicolon
+      , CloseBrace
+      ]
 
-invalidCases :: [Case]
-invalidCases = [
-  Case { description = "missing_paren.c"
-       , input       = "int main( {\n\
-                       \    return 0;\n\
-                       \}"
-       , expected    = [ KWInt
-                       , Identifier "main"
-                       , OpenParen
-                       , OpenBrace
-                       , KWReturn
-                       , Integer 0
-                       , Semicolon
-                       , CloseBrace
-                       ]
-       }
+  it "should lex no_brace.c" $ do
+    Lexer.lex "int main() {\n\
+              \    return 0;"
+      `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenParen
+      , CloseParen
+      , OpenBrace
+      , KWReturn
+      , Integer 0
+      , Semicolon
+      ]
 
-  , Case { description = "missing_retval.c"
-         , input       = "int main() {\n\
-                         \    return;\n\
-                         \}"
-         , expected    = [ KWInt
-                         , Identifier "main"
-                         , OpenParen
-                         , CloseParen
-                         , OpenBrace
-                         , KWReturn
-                         , Semicolon
-                         , CloseBrace
-                         ]
-         }
+  it "should lex no_semicolon.c" $ do
+    Lexer.lex "int main {\n\
+              \return 0\n\
+              \}"
+      `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenBrace
+      , KWReturn
+      , Integer 0
+      , CloseBrace
+      ]
 
-  , Case { description = "no_brace.c"
-         , input       = "int main() {\n\
-                         \    return 0;"
-         , expected    = [ KWInt
-                         , Identifier "main"
-                         , OpenParen
-                         , CloseParen
-                         , OpenBrace
-                         , KWReturn
-                         , Integer 0
-                         , Semicolon
-                         ]
-         }
-
-  , Case { description = "no_semicolon.c"
-         , input       = "int main {\n\
-                         \return 0\n\
-                         \}"
-         , expected    = [ KWInt
-                         , Identifier "main"
-                         , OpenBrace
-                         , KWReturn
-                         , Integer 0
-                         , CloseBrace
-                         ]
-         }
-
-  , Case { description = "no_space.c"
-         , input       = "int main() {\n\
-                         \    return0;\n\
-                         \}"
-         , expected    = [ KWInt
-                         , Identifier "main"
-                         , OpenParen
-                         , CloseParen
-                         , OpenBrace
-                         , Identifier "return0"
-                         , Semicolon
-                         , CloseBrace
-                         ]
-         }
-  ]
+  it "should lex no_space.c" $ do
+    Lexer.lex "int main() {\n\
+              \    return0;\n\
+              \}"
+      `shouldBe`
+      [ KWInt
+      , Identifier "main"
+      , OpenParen
+      , CloseParen
+      , OpenBrace
+      , Identifier "return0"
+      , Semicolon
+      , CloseBrace
+      ]
--- test/ParserSpec.hs
+++ test/ParserSpec.hs
@@ -1,9 +1,13 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module ParserSpec (spec) where
 
-import Data.Foldable     (for_)
-import Test.Hspec        (Spec, describe, it, shouldBe)
+import Control.Exception (evaluate)
+import Test.Hspec        ( Spec
+                         , describe
+                         , it
+                         , shouldBe
+                         , shouldThrow
+                         , errorCall
+                         )
 
 import Lexer (Token (..), lex)
 import Parser ( parse_
@@ -14,27 +18,26 @@ import Parser ( parse_
               )
 
 spec :: Spec
-spec = do describe "parser: valid" $ for_ validCases test
-  where
-    test Case {..} = it description $ parse_ input `shouldBe` expected
+spec = do
+  it "should parse tokens from multi_digit.c" $ do
+    parse_ [ KWInt
+           , Identifier "main"
+           , OpenParen
+           , CloseParen
+           , OpenBrace
+           , KWReturn
+           , Integer 100
+           , Semicolon
+           , CloseBrace] `shouldBe`
+      Program (Function "main" (Return (Constant 100)))
 
-data Case = Case { description :: String
-                 , input       :: [Token]
-                 , expected    :: Program
-                 }
-
-validCases :: [Case]
-validCases = [
-  Case { description = "multi_digit.c"
-       , input    = [ KWInt
-                    , Identifier "main"
-                    , OpenParen
-                    , CloseParen
-                    , OpenBrace
-                    , KWReturn
-                    , Integer 100
-                    , Semicolon
-                    , CloseBrace]
-       , expected = Program (Function "main" (Return (Constant 100)))
-       }
-  ]
+  it "should fail to parse tokens from missing_paren.c" $ do
+    evaluate (parse_ [ KWInt
+                     , Identifier "main"
+                     , OpenParen
+                     , OpenBrace
+                     , KWReturn
+                     , Integer 0
+                     , Semicolon
+                     , CloseBrace]) `shouldThrow`
+      errorCall "Failed to parse the program."
