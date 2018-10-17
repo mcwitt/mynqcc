abaa8ed502c4a2d16cdd7da8896ddc42661bc661
Matt Wittmann <mcwitt@gmail.com>
2018-10-17 15:04:13 -0700

    Generate code for unary ops

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -1,13 +1,20 @@
 module Codegen (generate) where
 
-import AST ( Program    (Program)
-           , Function   (Function)
-           , Statement  (Return)
-           , Expression (Constant))
+import AST
+import Text.Printf
+
+expression :: Expression -> String
+expression expr = case expr of
+  Constant ival          -> printf "movl $%d, %%eax\n" ival
+  Negation expr          -> expression expr ++ "neg %eax\n"
+  BitwiseComplement expr -> expression expr ++ "not %eax\n"
+  LogicalNegation expr   -> expression expr ++
+                            "cmpl $0, %eax\n\
+                            \movl $0, %eax\n\
+                            \sete %al\n"
 
 statement :: Statement -> String
-statement (Return (Constant ival)) =
-  "movl $" ++ show ival ++ ", %eax\nret"
+statement (Return expr) = expression expr ++ "ret"
 
 function :: Function -> String
 function (Function name body) =
--- test/CodegenSpec.hs
+++ test/CodegenSpec.hs
@@ -6,21 +6,36 @@ import Test.Hspec        ( Spec
                          , shouldBe
                          )
 
-import Codegen (generate)
-import Parser (parseTokens)
-
-import AST ( Program (Program)
-           , Function (Function)
-           , Statement (Return)
-           , Expression (Constant)
-           )
+import Codegen
+import Parser
+import AST
 
 spec :: Spec
 spec = do
   describe "Stage 1" $ do
+
     it "should generate code for multi_digit.c" $ do
-      generate (Program (Function "main" (Return (Constant 100)))) `shouldBe`
+      generate (Program (
+                   Function "main" (
+                       Return (Constant 100))))
+        `shouldBe`
         ".globl main\n\
         \main:\n\
         \movl $100, %eax\n\
+        \ret"
+
+  describe "Stage 2" $ do
+
+    it "should generate code for bitwise.c" $ do
+      generate (Program (
+                   Function "main" (
+                       Return (
+                           AST.LogicalNegation (Constant 12)))))
+        `shouldBe`
+        ".globl main\n\
+        \main:\n\
+        \movl $12, %eax\n\
+        \cmpl $0, %eax\n\
+        \movl $0, %eax\n\
+        \sete %al\n\
         \ret"
