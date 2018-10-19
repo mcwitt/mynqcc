edc145e11d1cc0afa9add368e815ccd387a0cf71
Matt Wittmann <mcwitt@gmail.com>
2018-10-19 12:20:32 -0700

    Refactor lexer

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -2,19 +2,61 @@ module Lexer ( lexString
              ) where
 
 import Data.Char
-import ParserCombinators ( Parser (..)
-                         , peek
-                         , empty
-                         , some
-                         , many
-                         , satisfy
-                         , atom
-                         , string
-                         , (<|>)
-                         )
+import ParserCombinators
+import Token
+import Error
 
-import Token ( Token (..) )
-import Error ( Error (LexerError) )
+lexString :: String -> Either Error [Token]
+lexString st = case parse lexer st of
+                 [(res, xs)] -> Right res
+                 [] -> Left $ LexerError "Failed to lex the program."
+
+lexer :: Parser Char [Token]
+lexer = many . token $ reservedWord "int" KWInt
+                   <|> reservedWord "return" KWReturn
+
+                   <|> charToken '{' OpenBrace
+                   <|> charToken '}' CloseBrace
+                   <|> charToken '(' OpenParen
+                   <|> charToken ')' CloseParen
+                   <|> charToken ';' Semicolon
+                   <|> charToken '-' Negation
+                   <|> charToken '~' BitwiseComplement
+                   <|> charToken '!' LogicalNegation
+                   <|> charToken '+' Addition
+                   <|> charToken '*' Multiplication
+                   <|> charToken '/' Division
+
+                   <|> identifier
+                   <|> integer
+
+token :: Parser Char a -> Parser Char a
+token p = do space
+             val <- p
+             return val
+
+charToken :: Char -> Token -> Parser Char Token
+charToken ch tok = do atom ch; return tok
+
+stringToken :: String -> Token -> Parser Char Token
+stringToken st tok = do string st; return tok
+
+reservedWord :: String -> Token -> Parser Char Token
+reservedWord st tok = do res <- stringToken st tok
+                         c <- peek
+                         if isAlphaNum c
+                           then empty
+                           else return res
+
+identifier :: Parser Char Token
+identifier = do s <- (do c <- letter
+                         cs <- many (letter <|> number)
+                         return (c:cs))
+                return $ Identifier s
+
+integer :: Parser Char Token
+integer = do s <- some number
+             return $ Integer (read s)
 
 number :: Parser Char Char
 number = satisfy isNumber
@@ -23,69 +65,6 @@ letter :: Parser Char Char
 letter = satisfy isLetter
 
 space :: Parser Char ()
-space = do many (satisfy isSpace)
+space = do many $ satisfy isSpace
            return ()
 
--- | Parser that consumes an identifier, defined as a string that matches the
--- | regex "[a-zA-Z][a-zA-Z0-9]*".
-ident :: Parser Char String
-ident = do c <- letter
-           cs <- many (letter <|> number)
-           return (c:cs)
-
--- | Transforms a parser to discard leading space.
-token :: Parser Char a -> Parser Char a
-token p = do space
-             val <- p
-             return val
-
-openBrace         = token (atom '{') >> return OpenBrace
-closeBrace        = token (atom '}') >> return CloseBrace
-openParen         = token (atom '(') >> return OpenParen
-closeParen        = token (atom ')') >> return CloseParen
-semicolon         = token (atom ';') >> return Semicolon
-negation          = token (atom '-') >> return Negation
-bitwiseComplement = token (atom '~') >> return BitwiseComplement
-logicalNegation   = token (atom '!') >> return LogicalNegation
-addition          = token (atom '+') >> return Addition
-multiplication    = token (atom '*') >> return Multiplication
-division          = token (atom '/') >> return Division
-
-
-keyword :: Parser Char a -> Parser Char a
-keyword p = do val <- p
-               c <- peek
-               if isAlphaNum c
-                 then empty
-                 else return val
-
-kwInt    = (token . keyword) (string "int")    >> return KWInt
-kwReturn = (token . keyword) (string "return") >> return KWReturn
-
-identifier = do s <- token ident
-                return (Identifier s)
-
-integer = do s <- token (some number)
-             return $ Integer (read s)
-
-lexer :: Parser Char [Token]
-lexer = many $  openBrace
-            <|> closeBrace
-            <|> openParen
-            <|> closeParen
-            <|> semicolon
-            <|> kwInt
-            <|> kwReturn
-            <|> identifier
-            <|> integer
-            <|> negation
-            <|> bitwiseComplement
-            <|> logicalNegation
-            <|> addition
-            <|> multiplication
-            <|> division
-
-lexString :: String -> Either Error [Token]
-lexString st = case parse lexer st of
-                 [(res, xs)] -> Right res
-                 [] -> Left $ LexerError "Failed to lex the program."
