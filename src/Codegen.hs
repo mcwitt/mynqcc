d54e434c170964f2ddcfda2c77c541d762ec8e5f
Matt Wittmann <mcwitt@gmail.com>
2018-11-11 12:33:41 -0800

    Refactor using mtl style

--- src/Codegen.hs
+++ src/Codegen.hs
@@ -1,3 +1,6 @@
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ConstraintKinds #-}
+
 module Codegen (generate) where
 
 import           AST
@@ -7,19 +10,9 @@ import           Control.Monad.Writer
 import qualified Data.Map             as Map
 import           Error
 
-
-{- | Inner monad type in which all code generation actions are executed. The
-inner Except monad allows for exception handling, and the WriterT keeps track of
-the assembly code generated so far.
--}
-type Gen = WriterT [String] (Except Error)
-
-{- | Inside functions, we need to keep track of some additional state: for
-example, the stack index and variable offsets relative to the base pointer
-(EBP). We wrap the base monad in a `StateT FunctionContext` to handle the
-additional bookkeeping.
--}
-type FuncGen = StateT Context Gen
+type MError  = MonadError Error
+type MWriter = MonadWriter [String]
+type MState  = MonadState Context
 
 data Context =
   Context { funcName   :: String
@@ -31,16 +24,16 @@ data Context =
 generate :: Program -> Either Error [String]
 generate = runExcept . execWriterT . program
 
-program :: Program -> Gen ()
+program :: (MWriter m, MError m) => Program -> m ()
 program (Program func) = function func
 
-function :: Function -> Gen ()
+function :: (MWriter m, MError m) => Function -> m ()
 function (Function name body) = do
   emit $ ".globl " ++ name
   emit $ name ++ ":"
   emit "push %ebp"
   emit "movl %esp, %ebp"
-  let inner = case (Function name body) of
+  let inner = case Function name body of
         -- Handle special case of empty main function
         Function "main" [] -> statement . Return . Constant $ 0
         Function _ body -> mapM_ blockItem body
@@ -53,7 +46,7 @@ function (Function name body) = do
   emit "pop %ebp"
   emit "ret"
 
-blockItem :: BlockItem -> FuncGen ()
+blockItem :: (MState m, MWriter m, MError m) => BlockItem -> m ()
 blockItem item = case item of
 
   Statement stat -> statement stat
@@ -67,13 +60,13 @@ blockItem item = case item of
     case maybeExpr of
       Just expr -> expression expr
       Nothing -> return ()
-    emitL "push %eax"
+    emit "push %eax"
     si <- gets stackIndex
     modify $ \ctx -> ctx { stackIndex = stackIndex ctx - 4
                          , varOffsets = Map.insert name si vars}
 
 
-statement :: Statement -> FuncGen ()
+statement :: (MState m, MWriter m, MError m) => Statement -> m ()
 statement st = case st of
 
   Return expr -> expression expr
@@ -81,47 +74,47 @@ statement st = case st of
 
   If expr s1 maybeStat -> do
     expression expr
-    emitL "cmpl $0, %eax"
+    emit "cmpl $0, %eax"
     labelElse <- label "else"
-    emitL $ "je " ++ labelElse
+    emit $ "je " ++ labelElse
     statement s1
     labelEndIf <- label "endif"
-    emitL $ "jmp " ++ labelEndIf
-    emitL $ labelElse ++ ":"
+    emit $ "jmp " ++ labelEndIf
+    emit $ labelElse ++ ":"
     case maybeStat of
       Just s2 -> statement s2
       Nothing -> return ()
-    emitL $ labelEndIf ++ ":"
+    emit $ labelEndIf ++ ":"
 
 
-expression :: Expression -> FuncGen ()
+expression :: (MState m, MWriter m, MError m) => Expression -> m ()
 expression expr = case expr of
 
-  Constant i -> emitL $ "movl $" ++ show i ++ ", %eax"
+  Constant i -> emit $ "movl $" ++ show i ++ ", %eax"
 
   Assignment name expr -> do
     expression expr
     vars <- gets varOffsets
     case Map.lookup name vars of
-      Just offset -> emitL $ "movl %eax, " ++ show offset ++ "(%ebp)"
+      Just offset -> emit $ "movl %eax, " ++ show offset ++ "(%ebp)"
       Nothing -> throwError . CodegenError $
                  "Assignment to undeclared variable, `" ++ name ++ "`."
 
   Reference name -> do
     vars <- gets varOffsets
     case Map.lookup name vars of
-      Just offset -> emitL $ "movl " ++ show offset ++ "(%ebp), %eax"
+      Just offset -> emit $ "movl " ++ show offset ++ "(%ebp), %eax"
       Nothing -> throwError . CodegenError $
                  "Reference to undeclared variable, `" ++ name ++ "`."
 
   Unary op expr -> do
     expression expr
-    case op of Negation -> emitL "neg %eax"
-               BitwiseComplement -> emitL "not %eax"
+    case op of Negation -> emit "neg %eax"
+               BitwiseComplement -> emit "not %eax"
                LogicalNegation -> do
-                 emitL "cmpl $0, %eax"
-                 emitL "movl $0, %eax"
-                 emitL "sete %al"
+                 emit "cmpl $0, %eax"
+                 emit "movl $0, %eax"
+                 emit "sete %al"
 
   {- General strategy for evaluating binary operators on sub-expressions e1, e2:
      1. Evaluate e1, push result onto the stack
@@ -130,76 +123,73 @@ expression expr = case expr of
      4. Perform the binary operation on e1, e2 -}
   Binary op e1 e2 -> do
     expression e2
-    emitL "push %eax"
+    emit "push %eax"
     expression e1
-    emitL "pop %ecx"
+    emit "pop %ecx"
     case op of
       Addition ->
-        emitL "addl %ecx, %eax"
+        emit "addl %ecx, %eax"
       Subtraction ->
-        emitL "subl %ecx, %eax"
+        emit "subl %ecx, %eax"
       Multiplication ->
-        emitL "imul %ecx, %eax"
+        emit "imul %ecx, %eax"
       Division -> do
-        emitL "movl $0, %edx"
-        emitL "idivl %ecx"
+        emit "movl $0, %edx"
+        emit "idivl %ecx"
       Equality -> do
-        emitL "cmpl %ecx, %eax"
-        emitL "movl $0, %eax"
-        emitL "sete %al"
+        emit "cmpl %ecx, %eax"
+        emit "movl $0, %eax"
+        emit "sete %al"
       Inequality -> do
-        emitL "cmpl %ecx, %eax"
-        emitL "movl $0, %eax"
-        emitL "setne %al"
+        emit "cmpl %ecx, %eax"
+        emit "movl $0, %eax"
+        emit "setne %al"
       LessThan -> do
-        emitL "cmpl %ecx, %eax"
-        emitL "movl $0, %eax"
-        emitL "setl %al"
+        emit "cmpl %ecx, %eax"
+        emit "movl $0, %eax"
+        emit "setl %al"
       GreaterThan -> do
-        emitL "cmpl %ecx, %eax"
-        emitL "movl $0, %eax"
-        emitL "setg %al"
+        emit "cmpl %ecx, %eax"
+        emit "movl $0, %eax"
+        emit "setg %al"
       LessEqual -> do
-        emitL "cmpl %ecx, %eax"
-        emitL "movl $0, %eax"
-        emitL "setle %al"
+        emit "cmpl %ecx, %eax"
+        emit "movl $0, %eax"
+        emit "setle %al"
       GreaterEqual -> do
-        emitL "cmpl %ecx, %eax"
-        emitL "movl $0, %eax"
-        emitL "setge %al"
+        emit "cmpl %ecx, %eax"
+        emit "movl $0, %eax"
+        emit "setge %al"
       LogicalOr -> do
-        emitL "orl %ecx, %eax"
-        emitL "movl $0, %eax"
-        emitL "setne %al"
+        emit "orl %ecx, %eax"
+        emit "movl $0, %eax"
+        emit "setne %al"
       LogicalAnd -> do
-        emitL "cmpl $0, %ecx"
-        emitL "setne %cl"
-        emitL "cmpl $0, %eax"
-        emitL "movl $0, %eax"
-        emitL "setne %al"
-        emitL "andb %cl, %al"
+        emit "cmpl $0, %ecx"
+        emit "setne %cl"
+        emit "cmpl $0, %eax"
+        emit "movl $0, %eax"
+        emit "setne %al"
+        emit "andb %cl, %al"
 
   Conditional e1 e2 e3 -> do
     expression e1
-    emitL "cmpl $0, %eax"
+    emit "cmpl $0, %eax"
     labelE3 <- label "e3"
-    emitL $ "je " ++ labelE3
+    emit $ "je " ++ labelE3
     expression e2
     labelPostCond <- label "post_conditional"
-    emitL $ "jmp " ++ labelPostCond
-    emitL $ labelE3 ++ ":"
+    emit $ "jmp " ++ labelPostCond
+    emit $ labelE3 ++ ":"
     expression e3
-    emitL $ labelPostCond ++ ":"
+    emit $ labelPostCond ++ ":"
 
-label :: String -> FuncGen String
+label :: MState m => String -> m String
 label s = do
   prefix <- gets funcName
   lc <- gets labelCount
   modify $ \ctx -> ctx { labelCount = succ lc }
   return $ "_" ++ prefix ++ "__" ++ s ++ "__" ++ show lc
 
-emit :: String -> Gen ()
-emit s = writer ((), [s])
-
-emitL :: String -> FuncGen ()
-emitL = lift . emit
+emit :: MWriter m => String -> m ()
+emit = tell . pure
